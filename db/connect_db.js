052ead38555fbe5f7438330cf3719df658847ee2
ritunjaya Saxena <ritunjayasaxena309@gmail.com>
2023-07-08 00:08:35 +0530

    aa

--- db/connect_db.js
+++ db/connect_db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose')
-const url ="mongodb://127.0.0.1:27017/user"
+// const url ="mongodb://127.0.0.1:27017/user"
 const live_url = "mongodb+srv://ritunjayasaxena309:khushi@cluster0.3lgflhk.mongodb.net/"
 
 const connectDB = ()=>{
